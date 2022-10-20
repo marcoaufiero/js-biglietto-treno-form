b883aebc8947da8557df8732690ad333a5ca45d5
marcoaufiero <marcoaufiero96@gmail.com>
2022-10-20 16:24:12 +0200

    html refresh button added

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,9 +4,6 @@ function displayTicket(){
     
     let userAge = parseInt(document.getElementById("inputAge").value);
 
-
-    // console.log(userAge, km)
-
     const priceKm = 0.21;
     
     let ticketPrice = parseFloat(km * priceKm).toFixed(2);
@@ -14,32 +11,27 @@ function displayTicket(){
     let ticketDiscount;
     
     if(userAge === 1){
+        
         ticketDiscount = (ticketPrice / 100) * 20;
     
-        console.log("1")
-
-    }else if(userAge === 3> 65){
+    }else if(userAge === 3){
+        
         ticketDiscount = (ticketPrice / 100) * 40;
-    
-        console.log("3")
-
+     
     }else{
         ticketDiscount = 0;
-
-        console.log("2")
+        
     }
     
-    let ticketDiscountAlert = parseFloat(ticketDiscount).toFixed(2);
+    ticketDiscount = parseFloat(ticketDiscount).toFixed(2);
     
     let finalPrice = parseFloat(ticketPrice - ticketDiscount).toFixed(2);
     
-    if(ticketDiscount !== 0){
-    
-        document.getElementById('ticketDiscountDisplay').innerHTML = `E' stato applicato uno sconto di € ${ticketDiscountAlert}`;
-    }
+    console.log(ticketDiscount)
+    console.log(finalPrice)
     
     document.getElementById('displayTicket').innerHTML = `Il prezzo del biglietto è € ${finalPrice}`;
 
-    // console.log(ticketDiscount)
+    console.log(ticketDiscount)
 
 }
